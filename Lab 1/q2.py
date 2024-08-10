6ab6a7d7c596a20d8b82dc5dbd401f2cdffa8f24
Harsh Agarwal <112351701+agarwalhafd22@users.noreply.github.com>
2024-08-10 15:13:17 +0530

    q2 complete

--- Lab 1/q2.py
+++ Lab 1/q2.py
@@ -28,9 +28,33 @@ cipherVigenere=""
 decryptedVigenere=""
 
 for i in range (n2):
-	cipherVigenere=cipherVigenere+chr((((ord(plainTextAS[i])+ord(keyNew[i])-97)%26)+97))
+	cipherVigenere=cipherVigenere+chr((((ord(plainTextAS[i])-97+ord(keyNew[i])-97)%26)+97))
 print("\nVigenere Cipher Text = "+cipherVigenere)
 
 for i in range (n2):
-	decryptedVigenere=decryptedVigenere+chr((((ord(cipherVigenere[i])-ord(keyNew[i])-97)%26)+97))
+	decryptedVigenere=decryptedVigenere+chr((((ord(cipherVigenere[i])-97-(ord(keyNew[i])-97))%26)+97))
 print("Vigenere Decrypted Text = "+decryptedVigenere)
+
+
+#Autokey Cipher
+
+
+autoKey=7
+cipherAutoKey=""
+cipherAutoKeyInter=""
+cipherAutoKeyInter=cipherAutoKeyInter+chr(7+97)
+for i in range (n2-1):
+	cipherAutoKeyInter=cipherAutoKeyInter+plainTextAS[i]
+
+for i in range (n2):
+	cipherAutoKey=cipherAutoKey+chr(((ord(plainTextAS[i])-97+ord(cipherAutoKeyInter[i])-97)%26)+97)
+
+print("\nAutokey Cipher Text = " + cipherAutoKey)
+
+decryptedAutoKey=""
+decryptedAutoKey=decryptedAutoKey+chr((ord(cipherAutoKey[0])-97-autoKey)+97)
+
+for i in range (1, n2):
+	decryptedAutoKey=decryptedAutoKey+chr(((ord(cipherAutoKey[i])-97-(ord(decryptedAutoKey[i-1])-97))%26)+97)
+
+print("Autokey Decrypted Text = "+decryptedAutoKey)
